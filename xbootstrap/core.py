31e7fd30a3632cd0ab0b74473dd9e66a3d96fde8
Doug Richardson <doug.richardson@csiro.au>
2022-08-24 16:59:00 +1000

    if statement for single object

--- xbootstrap/core.py
+++ xbootstrap/core.py
@@ -385,14 +385,22 @@ def block_bootstrap(*objects, blocks, n_iteration, exclude_dims=None):
             )
         )
 
-    return tuple(
-        [
-            xr.concat(
-                b,
-                dim="iteration",
-                coords="minimal",
-                compat="override",
-            )
-            for b in zip(*bootstraps)
-        ]
-    )
+    if len(objects) == 1:
+        return xr.concat(
+            *bootstraps,
+            dim="iteration",
+            coords="minimal",
+            compat="override",
+        )
+    else:
+        return tuple(
+            [
+                xr.concat(
+                    b,
+                    dim="iteration",
+                    coords="minimal",
+                    compat="override",
+                )
+                for b in zip(*bootstraps)
+            ]
+        )
